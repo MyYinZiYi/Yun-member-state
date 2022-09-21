f321998dd0630071ac8d756db7f2001d703dc6de
YZYmy <2832425415@qq.com>
2022-09-21 19:13:53 +0800

    feat(custom): 实现登录页面布局

--- src/request/request.js
+++ src/request/request.js
@@ -19,4 +19,15 @@ http.interceptors.response.use(function (response) {
     return Promise.reject(error);
 });
 
-export default http
+const request = (options) => {
+    options.method = options.method || "get"
+    if (options.method.toLowerCase() === "get") {
+        options.params = options.data || options.params
+        delete options.data
+    }
+    http.defaults.baseURL = options.proxy || process.env.VUE_APP_BASE_API
+
+    return http(options)
+}
+
+export default request
---
+++ src/views/login/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="Login-wrapper">
+    <div class="Login-form">
+        <h2 class="Login-title">积云会员管理系统</h2>
+      <el-form ref="form" :model="form" label-width="50px">
+        <el-form-item label="账号">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {},
+    };
+  },
+};
+</script>
+<style scoped>
+.Login-wrapper {
+  background: url("C:\Users\以往\Desktop\Yun-member-state\src\assets\preview.jpg")
+    no-repeat;
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.Login-form {
+  width: 350px;
+  background-color: rgba(255, 255, 255, 0.8);
+  margin: 160px auto;
+  height: 200px;
+  border-radius: 20px;
+  padding: 28px;
+}
+.Login-title{
+    font-size: 24px;
+    font-weight: 700;
+    text-align: center;
+}
+.el-form{
+    margin-top: 20px;
+}
+.el-button{
+    margin-right: 40px;
+}
+</style>
