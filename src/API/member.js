ead0d0cf823820bec2efd06aef3edfcec65b8055
YZYmy <2832425415@qq.com>
2022-09-25 19:58:25 +0800

    feat(custom): 实现会员管理删除功能

--- src/API/member.js
+++ src/API/member.js
@@ -1,28 +1,55 @@
 import request from "@/request/request"
 
 //获取会员列表与查询分页接口
-const getMemberList = (page,size,data) => {
+const getMemberList = (page, size, data) => {
 
-    return request({
-      url : `/member/list/search/${page}/${size}`,
-      method : "POST",
-      data
-    })
-  }
+  return request({
+    url: `/member/list/search/${page}/${size}`,
+    method: "POST",
+    data
+  })
+}
 
 //删除会员接口
+const deleteMemberList = (id) => {
+  return request({
+    url: `/member/${id}`,
+    method: 'DELETE'
+  })
+}
 
+// 新增会员接口
+const addMember = (data) => {
+  return request({
+    url: '/member',
+    method: 'POST',
+    data
+  })
+}
 
-//添加会员接口
+// 编辑会员接口
+const editMember = (id, data) => {
+  return request({
+    url: `/member/${id}`,
+    method: "PUT",
+    data
+  })
+}
 
-
-//编辑会员接口
-
-
-//查询单个会员接口
+// 查询单个会员接口
+const findMember = (id) => {
+  return request({
+    url: `/member/${id}`,
+    method: 'GET'
+  })
+}
 
 
 
 export default {
-    getMemberList,
+  getMemberList,
+  deleteMemberList,
+  addMember,
+  editMember,
+  findMember
 }
